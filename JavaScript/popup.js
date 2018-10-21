212eb9435ec8929307d228c44c6f8f7da7b154b4
Joel <jdbenge7@gmail.com>
2018-10-21 07:29:47 -0400

    Main page button commands to popup forms; prepped newsletter anchor tag.

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -1,20 +1,35 @@
-//Controls pop-up menu when "Submit a Recipe" Button is clicked.
+//Controls pop-up menu when "Submit a Recipe" or "Cotact Me" Buttons are clicked.
 
-const submitButton = document.getElementsByClassName("submit-recipe");
-const finalSubmit = document.getElementById("final-submit");
-const popup = document.getElementsByClassName("recipe-form");
+const submitInfo = document.getElementsByClassName("submit-info");
+const recipeSubmit = document.getElementById("recipe-submit");
+const contactMe = document.getElementById("contact-submit")
+const popup = document.getElementsByClassName("popup");
 
-submitButton[0].addEventListener('click', function(){
+//Listeners for "Submit a Recipe" and "Contant Me" Buttons
+submitInfo[0].addEventListener('click', function(){
+  if (popup[1].style.visibility != "visible") {
   popup[0].style.visibility = "visible";
-});
+}});
 
-finalSubmit.addEventListener('click', function(){
+submitInfo[1].addEventListener('click', function(){
+  if (popup[0].style.visibility != "visible") {
+  popup[1].style.visibility = "visible";
+}});
+
+//Handles each form when its "Submit" button is clicked.
+recipeSubmit.addEventListener('click', function(){
   popup[0].style.visibility = "hidden";
 });
 
+recipeSubmit.addEventListener('click', function(){
+  popup[1].style.visibility = "hidden";
+});
+
+//Clear Popup Forms with Escape
 document.onkeydown = function(evt) {
     evt = evt || window.event;
     if (evt.keyCode == 27) {
       popup[0].style.visibility = "hidden";
+      popup[1].style.visibility = "hidden";
     }
 };
