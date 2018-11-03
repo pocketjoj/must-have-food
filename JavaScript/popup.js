fe66eb73a584bad9238f5bd710e598f03be51974
Joel <jdbenge7@gmail.com>
2018-11-03 08:05:43 -0400

    Made modals so they'd disappear when clicked off of

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -4,6 +4,7 @@ const submitInfo = document.getElementsByClassName("submit-info");
 const recipeSubmit = document.getElementById("recipe-submit");
 const contactMe = document.getElementById("contact-submit")
 const popup = document.getElementsByClassName("popup");
+const popupForm = document.getElementsByClassName("popup-form");
 
 //Listeners for "Submit a Recipe" and "Contact Me" Buttons
 submitInfo[0].addEventListener('click', function(){
@@ -33,3 +34,19 @@ document.onkeydown = function(evt) {
       popup[1].style.visibility = "hidden";
     }
 };
+
+// Clear Popup Forms with Click Outside Forms??
+
+document.addEventListener('click', function(evt) {
+  const clickTarget = evt.target;
+  if (clickTarget == popupForm[0]) {}
+  else if (clickTarget == submitInfo[0]) {}
+  else {popup[0].style.visibility = "hidden";}
+})
+
+document.addEventListener('click', function(evt) {
+  const clickTarget = evt.target;
+  if (clickTarget == popupForm[1]) {}
+  else if (clickTarget == submitInfo[1]) {}
+  else {popup[1].style.visibility = "hidden";}
+})
