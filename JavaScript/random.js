d1014d9948a7a627fbe4e3e7958e6d2ab90e0395
Joel <jdbenge7@gmail.com>
2018-11-07 23:08:46 -0500

    Reset commits to original JS that redirected "surprise" link to recipe.

--- JavaScript/random.js
+++ JavaScript/random.js
@@ -1,27 +1,27 @@
 const random = Math.floor(Math.random()*12)+1;
 
 if (random == 1){
-  window.location.replace("../Recipes/Recipes/BeerBread.pdf");
+  window.location.replace("Recipes/BeerBread.pdf");
 } else if (random == 2){
-  window.location.replace("../Recipes/Recipes/BlackBeans.pdf");
+  window.location.replace("Recipes/BlackBeans.pdf");
 } else if (random == 3){
-  window.location.replace("../Recipes/Recipes/ChickenandRice.pdf");
+  window.location.replace("Recipes/ChickenandRice.pdf");
 } else if (random == 4){
-  window.location.replace("../Recipes/Recipes/ChickenParmesan.pdf");
+  window.location.replace("Recipes/ChickenParmesan.pdf");
 } else if (random == 5){
-  window.location.replace("../Recipes/Recipes/Fried-Chicken-Kale-Slaw.pdf");
+  window.location.replace("Recipes/Fried-Chicken-Kale-Slaw.pdf");
 } else if (random == 6){
-  window.location.replace("../Recipes/Recipes/FriedRice101.pdf");
+  window.location.replace("Recipes/FriedRice101.pdf");
 } else if (random == 7){
-  window.location.replace("../Recipes/Recipes/GUACAMOLE.pdf");
+  window.location.replace("Recipes/GUACAMOLE.pdf");
 } else if (random == 8){
-  window.location.replace("../Recipes/Recipes/OrzoPastaSalad.pdf");
+  window.location.replace("Recipes/OrzoPastaSalad.pdf");
 } else if (random == 9){
-  window.location.replace("../Recipes/Recipes/ParmesanCrustedChicken.pdf");
+  window.location.replace("Recipes/ParmesanCrustedChicken.pdf");
 } else if (random == 10){
-  window.location.replace("../Recipes/Recipes/redbeans.pdf");
+  window.location.replace("Recipes/redbeans.pdf");
 } else if (random == 11){
-  window.location.replace("../Recipes/Recipes/StuffedJalapenos.pdf");
+  window.location.replace("Recipes/StuffedJalapenos.pdf");
 } else {
-  window.location.replace("../Recipes/Recipes/ChickenandOrzo.pdf");
+  window.location.replace("Recipes/ChickenandOrzo.pdf");
 }
