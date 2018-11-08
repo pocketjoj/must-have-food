70ba844ec5e43b902a5a661e52089f9ba31c0b87
Joel <jdbenge7@gmail.com>
2018-11-07 23:00:19 -0500

    Set up "Surprise" page with buttons to surprise recipe or to refresh pg

--- JavaScript/random.js
+++ JavaScript/random.js
@@ -1,27 +1,28 @@
 const random = Math.floor(Math.random()*12)+1;
+const recipeLink = document.getElementById("recipe-link");
 
 if (random == 1){
-  window.location.href("../Recipes/Recipes/BeerBread.pdf");
+  recipeLink.href = "../Recipes/Recipes/BeerBread.pdf";
 } else if (random == 2){
-  window.location.href("../Recipes/Recipes/BlackBeans.pdf");
+  recipeLink.href = "../Recipes/Recipes/BlackBeans.pdf";
 } else if (random == 3){
-  window.location.href("../Recipes/Recipes/ChickenandRice.pdf");
+  recipeLink.href = "../Recipes/Recipes/ChickenandRice.pdf";
 } else if (random == 4){
-  window.location.href("../Recipes/Recipes/ChickenParmesan.pdf");
+  recipeLink.href = "../Recipes/Recipes/ChickenParmesan.pdf";
 } else if (random == 5){
-  window.location.href("../Recipes/Recipes/Fried-Chicken-Kale-Slaw.pdf");
+  recipeLink.href = "../Recipes/Recipes/Fried-Chicken-Kale-Slaw.pdf";
 } else if (random == 6){
-  window.location.href("../Recipes/Recipes/FriedRice101.pdf");
+  recipeLink.href = "../Recipes/Recipes/FriedRice101.pdf";
 } else if (random == 7){
-  window.location.href("../Recipes/Recipes/GUACAMOLE.pdf");
+  recipeLink.href = "../Recipes/Recipes/GUACAMOLE.pdf";
 } else if (random == 8){
-  window.location.href("../Recipes/Recipes/OrzoPastaSalad.pdf");
+  recipeLink.href = "../Recipes/Recipes/OrzoPastaSalad.pdf";
 } else if (random == 9){
-  window.location.href("../Recipes/Recipes/ParmesanCrustedChicken.pdf");
+  recipeLink.href = "../Recipes/Recipes/ParmesanCrustedChicken.pdf";
 } else if (random == 10){
-  window.location.href("../Recipes/Recipes/redbeans.pdf");
+  recipeLink.href = "../Recipes/Recipes/redbeans.pdf";
 } else if (random == 11){
-  window.location.href("../Recipes/Recipes/StuffedJalapenos.pdf");
+  recipeLink.href = "../Recipes/Recipes/StuffedJalapenos.pdf";
 } else {
-  window.location.href("../Recipes/Recipes/ChickenandOrzo.pdf");
+  recipeLink.href = "../Recipes/Recipes/ChickenandOrzo.pdf";
 }
