8adc66a52bf922072109e93ed447357a03a1358e
Joel <jdbenge7@gmail.com>
2018-11-07 23:03:18 -0500

    Built JavaScript for "Surprise" page to pull up random recipe (1 of 12)

---
+++ JavaScript/random.js
@@ -0,0 +1,27 @@
+const random = Math.floor(Math.random()*12)+1;
+
+if (random == 1){
+  window.location.replace("../Recipes/Recipes/BeerBread.pdf");
+} else if (random == 2){
+  window.location.replace("../Recipes/Recipes/BlackBeans.pdf");
+} else if (random == 3){
+  window.location.replace("../Recipes/Recipes/ChickenandRice.pdf");
+} else if (random == 4){
+  window.location.replace("../Recipes/Recipes/ChickenParmesan.pdf");
+} else if (random == 5){
+  window.location.replace("../Recipes/Recipes/Fried-Chicken-Kale-Slaw.pdf");
+} else if (random == 6){
+  window.location.replace("../Recipes/Recipes/FriedRice101.pdf");
+} else if (random == 7){
+  window.location.replace("../Recipes/Recipes/GUACAMOLE.pdf");
+} else if (random == 8){
+  window.location.replace("../Recipes/Recipes/OrzoPastaSalad.pdf");
+} else if (random == 9){
+  window.location.replace("../Recipes/Recipes/ParmesanCrustedChicken.pdf");
+} else if (random == 10){
+  window.location.replace("../Recipes/Recipes/redbeans.pdf");
+} else if (random == 11){
+  window.location.replace("../Recipes/Recipes/StuffedJalapenos.pdf");
+} else {
+  window.location.replace("../Recipes/Recipes/ChickenandOrzo.pdf");
+}
