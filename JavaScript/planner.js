8220c4578dbae01c91ad77c2ce03d68fc9790392
Joel <jdbenge7@gmail.com>
2018-11-08 19:02:25 -0500

    Put up placeholders on database and meal planner pages; favicon, etc.

---
+++ JavaScript/planner.js
@@ -0,0 +1,12 @@
+//Quick listener to display placeholder when shopping list button is clicked.
+
+const plannerButton = document.getElementsByTagName("button");
+const construction = document.getElementById("construction-pop");
+
+plannerButton[0].addEventListener('click', function(){
+  if (construction.style.display == "none"){
+      construction.style.display = "block"
+    } else {
+      construction.style.display = "none"
+    }
+});
